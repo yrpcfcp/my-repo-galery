9d27c9777022a15b7fdcd90274a2a0fce5af9b6a
yrpcfcp <67036488+yrpcfcp@users.noreply.github.com>
2022-06-07 17:50:30 -0300

    edited js file

--- js/script.js
+++ js/script.js
@@ -1,31 +1,95 @@
-const profileInformation = document.querySelector(".overview");
-const username = "yrpcfcp";
+// const profileInformation = document.querySelector(".overview");
+// const username = "yrpcfcp";
+// const repositoryList = document.querySelector(".repo-list")
 
-const gitInfo = async function () {
-    const response = await fetch(`https://api.github.com/users/${username}`);
-    const data = await response.json(); 
-    console.log(data)
-    userDataFetch(data)
+// const gitInfo = async function () {
+//     const response = await fetch(`https://api.github.com/users/${username}`);
+//     const data = await response.json(); 
+//     console.log(data)
+//     userDataFetch(data)
     
+// }
+
+// gitInfo()
+
+
+
+// function userDataFetch (data){
+//     let div = document.createElement("div");
+//     div.classList.add("user-info");
+//     div.innerHTML = `
+//     <figure>
+//       <img src=${data.avatar_url}/>
+//     </figure>
+//     <div>
+//       <p><strong>Name:</strong> ${data.name}</p>
+//       <p><strong>Bio:</strong> ${data.bio}</p>
+//       <p><strong>Location:</strong> ${data.location}</p>
+//       <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
+//     </div>`;
+//     profileInformation.append(div)
+// } 
+
+
+
+// userDataFetch()
+
+// const repositoryFetch = async function () {
+//     const response = await fetch("https://api.github.com/users/USERNAME/repos?page=1")
+//     const data = await response.json(); 
+//     console.log(data)
+//     repositoryFetch(data)
+  
+// }
+
+// repositoryFetch()
+
+const overview = document.querySelector(".overview");
+const username = "yrpcfcp";
+const repositoryList = document.querySelector(".repo-list")
+
+const gitUserInfo = async function () {
+  const userInfo = await fetch(`https://api.github.com/users/${username}`);
+  const data = await userInfo.json();
+  displayUserInfo(data);
+};
+
+gitUserInfo();
+
+const displayUserInfo = function (data) {
+  const div = document.createElement("div");
+  div.classList.add("user-info");
+  div.innerHTML = `
+    <figure>
+      <img alt="user avatar" src=${data.avatar_url} />
+    </figure>
+    <div>
+      <p><strong>Name:</strong> ${data.name}</p>
+      <p><strong>Bio:</strong> ${data.bio}</p>
+      <p><strong>Location:</strong> ${data.location}</p>
+      <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
+    </div>
+  `;
+  overview.append(div);
+};
+  
+const repositoryFetch = async function () {
+   const response = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`)
+   const data = await response.json(); 
+   console.log(data)
+   repositoryFetch(data)
+  
 }
 
-gitInfo()
+function reposInfo (repos){
+  for (repo of repos){
+    let repoListItem = document.createElement("li");
+    repoListItem.classList.add("repo");
+    repoListItem.innerHTML=`<h3>${repo.name}</h3>`;
 
+  }
+reposInfo.append(repoListItem);
+}
 
-
-function userDataFetch (data){
-    let div = document.createElement("div");
-    div.classList.add("userInfo")
-    div.innerHTML= `<figure><img alt="user avatar" src=${data.avatar_url} /></figure>
-    <div>
-    <p><strong>Name:</strong> ${data.name}</p>
-    <p><strong>Bio:</strong> ${data.bio}</p>
-    <p><strong>Location:</strong> ${data.location}</p>
-    <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
-    </div>`
-    profileInformation.append(div)
-} 
-
-
-
-userDataFetch()
+repositoryFetch()
+reposInfo()
