545a7483b244e68fc2d0db2a27aec1ad11102543
yrpcfcp <67036488+yrpcfcp@users.noreply.github.com>
2022-06-08 17:22:42 -0300

    .

--- js/script.js
+++ js/script.js
@@ -1,52 +1,8 @@
-// const profileInformation = document.querySelector(".overview");
-// const username = "yrpcfcp";
-// const repositoryList = document.querySelector(".repo-list")
-
-// const gitInfo = async function () {
-//     const response = await fetch(`https://api.github.com/users/${username}`);
-//     const data = await response.json(); 
-//     console.log(data)
-//     userDataFetch(data)
-    
-// }
-
-// gitInfo()
-
-
-
-// function userDataFetch (data){
-//     let div = document.createElement("div");
-//     div.classList.add("user-info");
-//     div.innerHTML = `
-//     <figure>
-//       <img src=${data.avatar_url}/>
-//     </figure>
-//     <div>
-//       <p><strong>Name:</strong> ${data.name}</p>
-//       <p><strong>Bio:</strong> ${data.bio}</p>
-//       <p><strong>Location:</strong> ${data.location}</p>
-//       <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
-//     </div>`;
-//     profileInformation.append(div)
-// } 
-
-
-
-// userDataFetch()
-
-// const repositoryFetch = async function () {
-//     const response = await fetch("https://api.github.com/users/USERNAME/repos?page=1")
-//     const data = await response.json(); 
-//     console.log(data)
-//     repositoryFetch(data)
-  
-// }
-
-// repositoryFetch()
-
 const overview = document.querySelector(".overview");
 const username = "yrpcfcp";
 const repoList = document.querySelector(".repo-list");
+const repositoriesContainer = document.querySelector(".repos");
+const repoData = document.querySelector(".repo-data");
 
 const gitUserInfo = async function () {
   const userInfo = await fetch(`https://api.github.com/users/${username}`);
@@ -89,3 +45,41 @@ const displayRepos = function (repos) {
   }
 };
 
+repoList.addEventListener("click", function (e) {
+  if (e.target.matches("h3")) {
+    const repoName = e.target.innerText;
+    getRepoInfo(repoName);
+  }
+});
+
+const getRepoInfo = async function (repoName) {
+  const fetchInfo = await fetch(`https://api.github.com/repos/${username}/${repoName}`);
+  const repoInfo = await fetchInfo.json();
+  console.log(repoInfo);
+  // Grab languages
+  const fetchLanguages = await fetch(repoInfo.languages_url);
+  const languageData = await fetchLanguages.json();
+
+  // Make a list of languages
+  const languages = [];
+  for (const language in languageData) {
+    languages.push(language);
+  }
+
+  displayRepoInfo(repoInfo, languages);
+};
+
+const displayRepoInfo = function (repoInfo, languages) {
+  repoData.innerHTML = "";
+  repoData.classList.remove("hide");
+  repositoriesContainer.classList.add("hide");
+  const div = document.createElement("div");
+  div.innerHTML = `
+    <h3>Name: ${repoInfo.name}</h3>
+    <p>Description: ${repoInfo.description}</p>
+    <p>Default Branch: ${repoInfo.default_branch}</p>
+    <p>Languages: ${languages.join(", ")}</p>
+    <a class="visit" href="${repoInfo.html_url}" target="_blank" rel="noreferrer noopener">View  on GitHub!</a>
+  `;
+  repoData.append(div);
+};
